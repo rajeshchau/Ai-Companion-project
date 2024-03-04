09a60f6f67c8de47c06547a81d443e53a82a95e6
rajesh chaudhari <93049758+rajeshchau@users.noreply.github.com>
2024-03-04 09:52:25 +0530

    (generated by blackbox.ai )

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
+import { Main } from "next/document";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +17,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+  return (<ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children}
+          </body>
+      </html>
+    </ClerkProvider>
   );
 }
